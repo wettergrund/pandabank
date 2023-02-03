d281af7555d05a7a04974e0bd301805940c4e69d
Toxicwhisper <morgan.lindzen@gmail.com>
2023-02-03 18:28:41 +0100

    Added method to convert currency from usd to sek

--- Bank/BankAccountModel.cs
+++ Bank/BankAccountModel.cs
@@ -18,6 +18,8 @@ namespace Bank
 
         public decimal balance { get; set; }
 
+        public decimal sek { get; set; }
+
         public double interest_rate { get; set; }
 
         public string currency_name { get; set; }
